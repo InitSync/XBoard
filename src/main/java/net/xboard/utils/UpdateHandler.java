ff2de622081a5d71910b19c7b4686c943a93d1a4
InitSync <benjaalejandro206@gmail.com>
2022-11-18 21:13:58 -0300

    feat(UpdateHandler): add update checker (v1.0.1)

---
+++ src/main/java/net/xboard/utils/UpdateHandler.java
@@ -0,0 +1,27 @@
+package net.xboard.utils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Scanner;
+import java.util.function.Consumer;
+
+public final class UpdateHandler {
+	private final int resourceNumber;
+	
+	public UpdateHandler(int resourceNumber) {
+		this.resourceNumber = resourceNumber <= 0
+			? -1
+			: resourceNumber;
+	}
+	
+	public void version(Consumer<String> version) {
+		try (InputStream stream = new URL("https://api.spigotmc/org/legacy/update.php?resource=" + resourceNumber).openStream()) {
+			Scanner scanner = new Scanner(stream);
+			if (scanner.hasNext()) version.accept(scanner.next());
+		} catch (IOException exception) {
+			LogUtils.error("Unable to check for updates available.");
+			exception.printStackTrace();
+		}
+	}
+}
