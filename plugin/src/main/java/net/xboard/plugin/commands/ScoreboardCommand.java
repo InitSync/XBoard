d71122c6f92f905fbe4cf951951426728d599ec7
InitSync <benjaalejandro206@gmail.com>
2022-12-28 17:13:16 -0300

    refactor(ScoreboardCommand): remove `Utils` deleted class

--- plugin/src/main/java/net/xboard/plugin/commands/ScoreboardCommand.java
+++ plugin/src/main/java/net/xboard/plugin/commands/ScoreboardCommand.java
@@ -1,11 +1,10 @@
 package net.xboard.plugin.commands;
 
-import com.cryptomorin.xseries.XSound;
 import net.xboard.api.handlers.ScoreboardHandler;
 import net.xboard.api.scoreboard.SimpleBoard;
+import net.xboard.api.utils.XSound;
 import net.xboard.plugin.enums.Permission;
 import net.xboard.plugin.utils.PlaceholderUtils;
-import net.xboard.plugin.utils.Utils;
 import net.xconfig.bukkit.config.BukkitConfigurationHandler;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -54,33 +53,16 @@ implements CommandExecutor {
 							 configurationHandler.number("", "config.yml", "config.sounds.volume-level")
 						);
 						player.sendMessage(configurationHandler.text("", "messages.yml", "messages.scoreboard-on", true).replace("<prefix>", prefix));
-						
-						Utils.sendTitle(
-							 player,
-							 PlaceholderUtils.parse(player, configurationHandler.text("", "messages.yml", "messages.scoreboard-title-on", true)),
-							 PlaceholderUtils.parse(player, configurationHandler.text("", "messages.yml", "messages.scoreboard-subtitle-on", true)),
-							 configurationHandler.number("", "config.yml", "config.titles.fade-in"),
-							 configurationHandler.number("", "config.yml", "config.titles.stay"),
-							 configurationHandler.number("", "config.yml", "config.titles.fade-out")
-						);
-					} else {
-						player.playSound(
-							 player.getLocation(),
-							 XSound.matchXSound(configurationHandler.text("", "config.yml", "config.sounds.scoreboard", false)).get().parseSound(),
-							 configurationHandler.number("", "config.yml", "config.sounds.volume-level"),
-							 configurationHandler.number("", "config.yml", "config.sounds.volume-level")
-						);
-						player.sendMessage(configurationHandler.text("", "messages.yml", "messages.scoreboard-off", true).replace("<prefix>", prefix));
-						
-						Utils.sendTitle(
-							 player,
-							 PlaceholderUtils.parse(player, configurationHandler.text("", "messages.yml", "messages.scoreboard-title-on", true)),
-							 PlaceholderUtils.parse(player, configurationHandler.text("", "messages.yml", "messages.scoreboard-subtitle-on", true)),
-							 configurationHandler.number("", "config.yml", "config.titles.fade-in"),
-							 configurationHandler.number("", "config.yml", "config.titles.stay"),
-							 configurationHandler.number("", "config.yml", "config.titles.fade-out")
-						);
+						return false;
 					}
+					
+					player.playSound(
+						 player.getLocation(),
+						 XSound.matchXSound(configurationHandler.text("", "config.yml", "config.sounds.scoreboard", false)).get().parseSound(),
+						 configurationHandler.number("", "config.yml", "config.sounds.volume-level"),
+						 configurationHandler.number("", "config.yml", "config.sounds.volume-level")
+					);
+					player.sendMessage(configurationHandler.text("", "messages.yml", "messages.scoreboard-off", true).replace("<prefix>", prefix));
 					break;
 				case "title":
 					if (configurationHandler.condition("", "config.yml", "config.scoreboard.allow-animated-title")) {
